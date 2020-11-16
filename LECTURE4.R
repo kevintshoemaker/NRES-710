db8f1b4db59956eaec1d8cefc6bb88e29a10545a
Kevin <kevintshoemaker@gmail.com>
2020-11-15 16:12:46 -0800

    ANOVA

--- LECTURE4.R
+++ LECTURE4.R
@@ -114,3 +114,171 @@ plot(anscombe$y2~anscombe$x2,ylab="response",xlab="predictor")
 plot(anscombe$y3~anscombe$x3,ylab="response",xlab="predictor")
 plot(anscombe$y4~anscombe$x4,ylab="response",xlab="predictor")
 
+
+## load the 'mtcars' data set
+
+data(mtcars)
+
+## define which variables to assess correlation for.
+
+myvars <- c("disp","hp","wt")
+
+## grab only the variables of interest
+
+mtcars.reduced <- mtcars[,myvars]
+
+## compute (pearson) correlations for all pairs of variables (correlation matrix)
+
+cor.mat <- cor(mtcars.reduced)
+
+cor.mat
+
+## visualize correlations with the 'pairs' function
+
+pairs(mtcars.reduced)
+
+## run a correlation test- with 95% confidence interval
+    # default is Pearson product-moment correlation.
+
+cor.test(mtcars$disp,mtcars$wt)
+
+## now try a non-parametric version
+
+cor.test(mtcars$disp,mtcars$wt, method = "kendall") # or spearman
+
+
+
+########
+# load the car package
+
+library(car)
+
+#######
+# load the trees dataset
+
+data(trees)
+
+######
+# visualize relationships among predictor vars
+
+pairs(trees[,c("Girth","Height")])
+
+
+######
+# check for correlation in predictor variables
+
+cor(trees[,c("Girth","Height")])   # predictor variabes not highly correlated
+
+# run a multiple regression model
+
+my.mod <- lm(Volume~Girth+Height,data=trees)
+
+
+# check variance inflation factors
+
+car::vif(my.mod)
+
+
+########
+## mtcars multicollinearity example
+
+mymod <- lm(mpg~disp+hp+wt,data=mtcars) 
+
+vif(mymod)
+
+
+cor(mtcars.reduced)
+
+## let's remove the 'disp' variable and keep weight...
+
+mymod <- lm(mpg~hp+wt,data=mtcars) 
+
+vif(mymod)
+
+
+#### ANOVA as regression example
+
+data(iris)    # load the iris dataset
+
+plot(iris$Sepal.Length ~ iris$Species)   # r uses a boxplot by default for categorical predictor
+
+my.mod <- lm(Sepal.Length~Species,data=iris)    # run an ANOVA!
+summary(my.mod)    # look at the results
+
+anova(my.mod)     # produce an analysis of variance table
+
+####
+# alternative!
+
+my.mod <- aov(Sepal.Length~Species,data=iris)   # same model!!
+summary(my.mod)     # but produces an anova table by default
+
+
+
+
+# ?nls
+
+data(DNase)
+
+plot(DNase$density~DNase$conc)   # looks non-linear!
+
+### run linear regression model
+
+model1 <- lm(density~conc,data=DNase)
+
+### run diagnostic plots
+
+par(mfrow=c(2,2))
+plot(model1)     # clear non-linearity (and non-normal residuals, and heteroskedasticity!)
+
+par(mfrow=c(1,1))      # plot data with regression line - obvious issues!
+plot(DNase$density~DNase$conc)
+abline(model1)
+
+### run non-linear regression model - use saturation curve
+
+model2 <- nls(density ~ (max*conc)/(K+conc),data=DNase,start=list(max=2,K=0.25))
+summary(model2)
+
+### run non-linear regression model - use logistic function 
+
+model3 <- nls(density ~ Asym/(1 + exp((xmid - log(conc))/scal)),
+                 data = DNase,
+                 start = list(Asym = 3, xmid = 0, scal = 1))
+
+summary(model3)
+
+plot(DNase$density~DNase$conc)
+abline(model1)
+concs.toplot <- seq(0.01,14,0.1)
+densities.toplot <- predict(model2,newdata=data.frame(conc=concs.toplot))
+lines(concs.toplot,densities.toplot,col="blue")
+densities.toplot <- predict(model3,newdata=data.frame(conc=concs.toplot))
+lines(concs.toplot,densities.toplot,col="red")
+
+legend("topleft",lty=c(1,1,1),col=c("black","blue","red"),legend=c("linear",
+                  "saturation","logistic"))
+
+
+##########
+# heteroskedasticity
+
+##### first, simulate data with heteroskedastic residuals
+
+simulated.x <- runif(100,0.1,5)
+simulated.y <- exp(rnorm(100,1.1+0.3*simulated.x,0.7))
+
+plot(simulated.y~simulated.x)
+
+### run linear model
+
+model1 <- lm(simulated.y~simulated.x)
+par(mfrow=c(2,2))
+plot(model1)  # run diagnostic plots    heteroskedasticity issues
+
+### run linear model with log transformation of response variable
+
+model2 <- lm(log(simulated.y)~simulated.x)
+par(mfrow=c(2,2))
+plot(model2)  # run diagnostic plots - no issues!  
+
--- LECTURE5.R
+++ LECTURE5.R
@@ -14,3 +14,190 @@
 ############################################################
 
 
+
+#######
+# Simple one-way ANOVA example
+
+F1 <- c(1,2,2,3)     # plant height under fertilizer treatment 1
+F2 <- c(5,6,5,4)
+F3 <- c(2,1,2,2)
+
+# combine into single dataframe for easier visualization and analysis
+
+df <- data.frame(
+  Height = c(F1,F2,F3),
+  Treatment = rep(c("F1","F2","F3"),each=length(F1)),
+  stringsAsFactors = T
+)
+
+plot(Height~Treatment, data=df)
+
+
+
+grand.mean <- mean(df$Height)   # grand mean
+
+group.means <- by(df$Height,df$Treatment,mean)    # group means
+
+n.groups <- length(group.means)   # number of groups
+
+group.sample.size <- by(df$Height,df$Treatment,length)
+
+sample.size <- nrow(df)
+
+explained.var <- sum(group.sample.size*(group.means-grand.mean)^2/(n.groups-1))
+
+groups <- lapply(1:n.groups,function(t) df$Height[df$Treatment==levels(df$Treatment)[t]])
+
+residual.var <- sapply(1:n.groups,function(t) (groups[[t]]-group.means[t])^2/(sample.size-n.groups) )
+
+unexplained.var <- sum(residual.var)
+
+#######
+# now we can compute the F statistic!
+
+Fstat <- explained.var/unexplained.var
+Fstat
+
+
+#######
+# define degrees of freedom
+
+df1 <- n.groups-1
+df2 <- sample.size-n.groups
+
+#######
+# visualize the sampling distribution under null hypothesis
+
+curve(df(x,df1,df2),0,10)
+
+
+######
+# compute critical value of F statistic
+
+Fcrit <- qf(0.95,df1,df2)
+Fcrit
+
+######
+# compute p-value
+
+pval <- 1-pf(Fstat,df1,df2)
+
+
+#####
+# use aov function
+
+model1 <- aov(Height~Treatment,data=df)
+summary(model1)
+
+#####
+# use lm function
+
+model1 <- lm(Height~Treatment,data=df)
+summary(model1)
+
+anova(model1)
+
+
+
+######
+# Tukey's test
+
+# find critical q-value for tukey test
+q.value <- qtukey(p=0.95,nmeans=n.groups,df=(sample.size-n.groups))
+
+# find honestly significant difference
+tukey.hsd <- q.value * sqrt(unexplained.var/(sample.size/n.groups))
+
+# if differences in group means are greater than this value then we can reject the null!
+
+## let's look at the difference between means
+
+all_means <- tapply(df$Height,df$Treatment,mean)
+all_levels <- levels(df$Treatment)
+pair_totry <- matrix(c(1,2,1,3,2,3),nrow=3,byrow = T)
+pair_totry     # these are the pairwise comparisons to make!
+
+thispair <- pair_totry[1,]    # run first pairwise comparison
+
+dif.between.means <- all_means[thispair[1]]-all_means[thispair[2]]
+dif.between.means   # since this is greater than tukey.hsd, we already know we can reject the null
+
+### compute p-value!
+sample.size.pergroup <- sample.size/n.groups
+std.err <- sqrt(unexplained.var / 2 * (2 / sample.size.pergroup))
+
+ # first compute q statistic
+q.stat <- abs(dif.between.means)/std.err
+p.val <- 1-ptukey(q.stat,nmeans=n.groups,df=(sample.size-n.groups))
+p.val
+
+## run all pairwise comparisons
+
+results <- NULL
+i=1
+for(i in 1:nrow(pair_totry)){
+  thispair <- pair_totry[i,]
+  temp <- data.frame(
+    group1 = all_levels[thispair[1]],
+    group1 = all_levels[thispair[2]]
+  )
+  
+  temp$dif = all_means[thispair[1]]-all_means[thispair[2]]
+  temp$qstat = abs(temp$dif)/std.err
+  temp$pval = 1-ptukey(temp$qstat,nmeans=n.groups,df=(sample.size-n.groups))
+  
+  results <- rbind(results,temp)
+}
+
+results
+
+
+########  compare with R's built in tukey test function
+model1 <- aov(Height~Treatment,data=df)
+TukeyHSD(model1)
+
+
+#######  and finally, compare with 'emmeans'
+
+library(emmeans)
+model1 <- lm(Height~Treatment,data=df)
+emm <- emmeans(model1,specs=c("Treatment"))  # compute the treatment means with 'emmeans'
+pairs(emm)    # run tukey test!
+
+
+library(agricolae)
+data("PlantGrowth")
+
+plant.lm <- lm(weight ~ group, data = PlantGrowth)   #run the 'regression' model
+plant.av <- aov(plant.lm)  # run anova test and print anova table
+plant.av
+
+#######
+# evaluate goodness of fit (assumption violations etc)
+layout(matrix(1:4,nrow=2,byrow = T))
+plot(plant.av)
+
+#######
+# run pairwise comparisons
+
+tukeytest <- TukeyHSD(plant.av)
+tukeytest
+
+layout(matrix(1,nrow=1,byrow = T))
+plot(tukeytest)   #default plotting method for tukey test objects!
+
+
+######
+# alternative method
+
+
+ # run tukey test
+emm <- emmeans(plant.lm,specs=c("group"))  # compute the treatment means with 'emmeans'
+pairs(emm)    # run tukey test!
+
+toplot <- as.data.frame(summary(emm))[,c("group","emmean","lower.CL","upper.CL")]
+xvals <- barplot(toplot$emmean,names.arg = toplot$group,ylim=c(0,7))
+arrows(xvals,toplot$lower.CL,xvals,toplot$upper.CL,angle=90,code=3)
+text(xvals,c(6.4,6.4,6.4),labels = c("ab","a","b"),cex=1.5)
+
+
